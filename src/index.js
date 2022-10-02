63593bf806658d75df891a7779cc3e9d27d3cc4f
Asad Bashir <asad.bashir2580@gmail.com>
2022-10-02 02:37:21 -0400

    added cors headers

--- src/index.js
+++ src/index.js
@@ -44,7 +44,13 @@ const corsConfig = {
 };
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.get("Origin") || "*");
+  // res.header("Access-Control-Allow-Origin", req.get("Origin") || "*");
+  res.header("Access-Control-Allow-Origin", "blogcity-api.onrender.com");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 app.use(express.static(__dirname));
